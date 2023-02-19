1af3dfe3c4afce67aab240c6e688b625caf3ebd2
ThibautMol <thibautm@hotmail.fr>
2023-02-19 17:57:25 +0100

    Maj

--- Projet_générateur_automatique_exercices_de_sport.py
+++ Projet_générateur_automatique_exercices_de_sport.py
@@ -1,34 +1,201 @@
-"""Projet générateur automatique exercices de sport"""
+#!/usr/bin/python3
+# coding: utf-8
 
-def main():
-    print("1 - Créer une recette")
-    print("2 - Charger une recette")
+""""creer 3 fonctions : 
+1- proposer niveau difficulte
+2- determiner dans quoi on va sortir un sample d'exo
+3- determiner les repetitions en fonction du niveau de difficulte"""
 
-    input_utilisateur = input("Que voulez-vous faire ? ")
+import random
+from random import sample
 
-    if input_utilisateur == "1":
-        creer_recette()
-    elif input_utilisateur == "2":
-        nom = input("Nom de la recette ? ")
-        charger_recette(nom)
 
-if __name__ == "__main__":
-    main()
+"""difficulty_level_1=False
+difficulty_level_2=False
+difficulty_level_3=False"""
 
-    """demander à l'utilisateur combien de zone du corps il veut cibler (bas du corps, haut du corps, abdos, cardio). 
-    l'utilisateur peut choisir entre 1 et 4 type d'exo. s'il tape 0, renvoyer "merci au revoir" et terminer le programme.
-    demander quel exercice il veut et compter. quand le nombre de type d'exo est choisi, on passe à la prochaine étape.
-    afficher la liste : 
-    1 exercice bas du corps
-    2 exercices abdos
-    3 exercice haut du corps
-    4 cardio
-    demander combien l'utilisateur veut d'exercices
-    lorsqu'un nombre est choisi la phrase "quel type d'exercice choisir" revient. 
-    faire un décompte en fonction du nombre de type d'exercice choisi. Quand ça arrive à 0 on change d'étape.
-    enregistrer l'ordre et le type d'exo dans un fichier a côté
-    lancer un choix random de chiffres en fonction du nombre d'exercices enregistrés pour déterminer les exo à faire.
-    Créer une alternance entre un exo de chaque type choisi (bas du corps, haut du corps, abdos, etc) pour ne pas avoir deux exercices qui se suivent sur la même zone. 
-    sortir un résumé des exo choisis avec le nom de chacuns 
-          
-    """
+exercices_upper_body = [{
+    "nom": "élévation laterale",
+    "nbr_repet": 10},
+    {"nom": "curl biceps",
+    "nbr_repet": 10},
+    {"nom": "tractions",
+    "nbr_repet": 5},
+    {"nom": "pompes",
+    "nbr_repet": 5}]
+
+exercices_abdo_and_legs = [{
+    "nom": "crunch",
+    "nbr_repet": 20},
+    {"nom": "gainage planche",
+    "nbr_repet": 20},
+    {"nom": "gainage lateral droit",
+    "nbr_repet": 20},
+    {"nom": "gainage lateral gauche",
+    "nbr_repet": 20},
+    {"nom": "superman",
+    "nbr_repet": 20},
+    {"nom" : "squats",
+    "nbr_repet" : 20},
+    {"nom" : "fentes avant alternées",
+    "nbr_repet" : 20},
+    {"nom" : "fentes arrière alternées",
+    "nbr_repet" : 20}]
+
+exercices_cardio = [{
+    "nom" : "jumping jack",
+    "nbr_repet": 50}, 
+    {"nom" : "mountain climber",
+    "nbr_repet": 30}, 
+    {"nom" : "monte de genoux",
+    "nbr_repet": 50},
+    {"nom": "squats sautés",
+    "nbr_repet": 20},
+    {"nom": "corde à sauter",
+    "nbr_repet": 100}]
+
+exercices_upper_body_choose = []
+exercices_abdo_and_legs_choose = []
+exercices_cardio_choose = []
+
+seance = []
+
+"""
+def difficulty_level(difficulty) :
+    
+    if difficulty == 1 :
+        difficulty_level_1 = True
+        
+    elif difficulty == 2 :
+         difficulty_level_2 = True
+
+    elif difficulty == 3 :
+         difficulty_level_3 = True
+
+    else :
+        print("error, value invalid")
+"""
+
+
+def choose_exercices_upper_body(nbr):
+    global exercices_upper_body_choose
+    exercices_upper_body_choose=random.sample(exercices_upper_body, nbr)
+    print(exercices_upper_body_choose)
+
+
+def choose_exercices_abdo_and_legs(nbr):
+    global exercices_abdo_and_legs_choose
+    exercices_abdo_and_legs_choose=random.sample(exercices_abdo_and_legs, nbr)
+    print(exercices_abdo_and_legs_choose)
+     
+
+
+def choose_exercices_cardio(nbr):
+    global exercices_cardio_choose
+    exercices_cardio_choose=random.sample(exercices_cardio, nbr)
+    print(exercices_cardio_choose)   
+
+
+difficulty = int(input("Choisissez votre niveau de difficulté entre 1, 2 et 3 : "))
+
+
+nbr_upper_body_exercices = int(input
+                               (f"Précisez combien d'exercices vous souhaitez pour le haut du corps entre 1 et {len(exercices_upper_body)}:"))
+
+choose_exercices_upper_body(nbr_upper_body_exercices)
+
+
+nbr_abdos_exercices = int(input
+                          (f"Précisez combien d'exercices vous souhaitez pour les abdos entre 1 et {len(exercices_abdo_and_legs)} :"))
+
+choose_exercices_abdo_and_legs(nbr_abdos_exercices)
+
+
+nbr_cardio_exercices = int(input
+                           (f"Précisez combien d'exercices vous souhaitez pour le cardio entre 1 et {len(exercices_cardio)} :"))
+
+choose_exercices_cardio(nbr_cardio_exercices)
+
+
+
+for exo in exercices_upper_body_choose:
+    seance.append(
+        {
+            "nom": exo,
+            "nombre_répétitions": "nbr_repet"*difficulty
+        }
+    )
+
+for exo in exercices_abdo_and_legs_choose:
+    seance.append(
+        {
+            "nom": exo,
+            "nombre_répétitions": "nbr_repet"*difficulty
+        }
+    )
+
+for exo in exercices_cardio_choose:
+    seance.append(
+        {
+            "nom": exo,
+            "nombre_répétitions": "nbr_repet"*difficulty
+        }
+    )
+
+
+"""scratch 1 :
+import csv
+field_names= ['nom','nbr_repet']
+difficulty=2
+data=[]
+
+
+list1=[{"nom": "bernard", "nbr_repet": int(20)},
+{"nom" : "bianca", "nbr_repet" : int(10)},
+{"nom" : "henry", "nbr_repet" : int(15)}]
+
+#list1["nbr_repet"]=int(*difficulty)
+
+with open('routine.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=field_names)
+    writer.writeheader()
+    writer.writerows(list1)
+
+with open ("routine.csv") as donnees :
+    reader = csv.DictReader(donnees, delimiter=',')
+    for ligne in reader :
+        nom=str(ligne['nom'])
+        nbr_repet=int(nbr_repet*difficulty)
+        print ("nom", nom, "nbr", nbr_repet)
+    nom=(ligne['nom'])
+    data.append(ligne["nom"] + " " + nbr_repet)
+
+    print(data)"""
+
+"""scratch 2 :
+
+resultat=[]
+difficulty=int(2)
+liste=[
+    {"nom": "bob",
+     "nbr_repet" : int(10)},
+     {"nom" : "bianca",
+      "nbr_repet" : int(15)},
+      {"nom" : "henri",
+       "nbr_repet" : int(22)}
+]
+x=0
+for i in range (0, (len(liste))) :
+
+    resultat.append(liste[x])
+    x+=1
+
+print(resultat)
+
+resultat["nbr_repet"]=int(20)
+#int(*difficulty)
+
+
+#nom["nom"]=int(10*difficulty)
+
+print(resultat)"""
